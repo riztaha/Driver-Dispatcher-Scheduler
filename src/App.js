240c1070cbc8bf930b19abfc03b5ad367d65d3b8
Taha Rizvi <riztaha@gmail.com>
2020-07-27 17:15:52 -0400

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  MenuItem,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Nav className="mr-auto">
+          <Nav.Link href="#Driver-1">Driver 1 Schedule</Nav.Link>
+          <Nav.Link href="#Driver-2">Driver 2 Schedule</Nav.Link>
+          <Nav.Link href="#Driver-3">Driver 3 Schedule</Nav.Link>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
