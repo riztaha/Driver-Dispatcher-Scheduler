cae2f058f5807eaaed31ef4bf886dee43cdf3dc3
Taha Rizvi <riztaha@gmail.com>
2020-07-28 13:13:40 -0400

    Testing calendar library

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,7 +1,52 @@
 import React from "react";
 import AvailableTimes from "react-available-times";
 
+import Timeline from "react-calendar-timeline";
+// make sure you include the timeline stylesheet or the timeline will not be styled
+import "react-calendar-timeline/lib/Timeline.css";
+import moment from "moment";
+import CustomTimeline from "./CustomTimeline";
+
 function Calendar() {
+  //   const groups = [
+  //     { id: 1, title: "Michael Scott" },
+  //     { id: 2, title: "Jim Halpert" },
+  //     { id: 3, title: "Dwight Schrute" },
+  //   ];
+
+  //   const items = [
+  //     {
+  //       id: 1,
+  //       group: 1,
+  //       title: "Pickup",
+  //       start_time: moment(),
+  //       end_time: moment().add(1, "hour"),
+  //     },
+  //     {
+  //       id: 2,
+  //       group: 2,
+  //       title: "Other",
+  //       start_time: moment().add(-1.5, "hour"),
+  //       end_time: moment().add(0.5, "hour"),
+  //     },
+  //     {
+  //       id: 3,
+  //       group: 1,
+  //       title: "Dropoff",
+  //       start_time: moment().add(2, "hour"),
+  //       end_time: moment().add(3, "hour"),
+  //     },
+  //   ];
+  //   return (
+  //     <div>
+  //       {/* <CustomTimeline /> */}
+  //       <Timeline
+  //         groups={groups}
+  //         items={items}
+  //         defaultTimeStart={moment().add(-12, "hour")}
+  //         defaultTimeEnd={moment().add(12, "hour")}
+  //       />
+  //     </div>
   return (
     <div>
       <AvailableTimes
@@ -17,13 +62,13 @@ function Calendar() {
           {
             id: "dropoff",
             title: "Dropoff",
-            foregroundColor: "#66776",
+            foregroundColor: "#ff00ff",
             backgroundColor: "#f3f3f3",
           },
           {
             id: "other",
             title: "Other",
-            foregroundColor: "#666",
+            foregroundColor: "#ff00ff",
             backgroundColor: "#f3f3f3",
           },
         ]}
---
+++ src/CustomTimeline.jsx
@@ -0,0 +1,100 @@
+// import React, { Component } from "react";
+// import moment from "moment";
+
+// import Timeline from "react-calendar-timeline";
+
+// import generateFakeData from "./generate-fake-data";
+
+// var keys = {
+//   groupIdKey: "id",
+//   groupTitleKey: "title",
+//   groupRightTitleKey: "rightTitle",
+//   itemIdKey: "id",
+//   itemTitleKey: "title",
+//   itemDivTitleKey: "title",
+//   itemGroupKey: "group",
+//   itemTimeStartKey: "start",
+//   itemTimeEndKey: "end",
+//   groupLabelKey: "title"
+// };
+
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     const { groups, items } = generateFakeData();
+//     const defaultTimeStart = moment()
+//       .startOf("day")
+//       .toDate();
+//     const defaultTimeEnd = moment()
+//       .startOf("day")
+//       .add(1, "day")
+//       .toDate();
+
+//     this.state = {
+//       groups,
+//       items,
+//       defaultTimeStart,
+//       defaultTimeEnd
+//     };
+//   }
+
+//   handleItemMove = (itemId, dragTime, newGroupOrder) => {
+//     const { items, groups } = this.state;
+
+//     const group = groups[newGroupOrder];
+
+//     this.setState({
+//       items: items.map(item =>
+//         item.id === itemId
+//           ? Object.assign({}, item, {
+//               start: dragTime,
+//               end: dragTime + (item.end - item.start),
+//               group: group.id
+//             })
+//           : item
+//       )
+//     });
+
+//     console.log("Moved", itemId, dragTime, newGroupOrder);
+//   };
+
+//   handleItemResize = (itemId, time, edge) => {
+//     const { items } = this.state;
+
+//     this.setState({
+//       items: items.map(item =>
+//         item.id === itemId
+//           ? Object.assign({}, item, {
+//               start: edge === "left" ? time : item.start,
+//               end: edge === "left" ? item.end : time
+//             })
+//           : item
+//       )
+//     });
+
+//     console.log("Resized", itemId, time, edge);
+//   };
+
+//   render() {
+//     const { groups, items, defaultTimeStart, defaultTimeEnd } = this.state;
+
+//     return (
+//       <Timeline
+//         groups={groups}
+//         items={items}
+//         keys={keys}
+//         fullUpdate
+//         itemTouchSendsClick={false}
+//         stackItems
+//         itemHeightRatio={0.75}
+//         canMove={true}
+//         canResize={"both"}
+//         defaultTimeStart={defaultTimeStart}
+//         defaultTimeEnd={defaultTimeEnd}
+//         onItemMove={this.handleItemMove}
+//         onItemResize={this.handleItemResize}
+//       />
+//     );
+//   }
+// }
