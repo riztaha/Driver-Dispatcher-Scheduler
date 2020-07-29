3ab5d14e9248454952b4005b0512099ae0e60f18
Taha Rizvi <riztaha@gmail.com>
2020-07-29 19:43:06 -0400

    Now able to choose 1/3 drivers and create timeslot. Also added CSV functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import Calendar from "./Calendar";
+import Nav from "./Nav";
+import "react-calendar-timeline/lib/Timeline.css";
+import moment from "moment";
+import { CSVLink } from "react-csv";
+
+let groups = [
+  { id: "1", title: "Michael", rightTitle: "Scott", bgColor: "#f9b87a" },
+  { id: "2", title: "Dwight", rightTitle: "Schrute", bgColor: "#f7d788" },
+  { id: "3", title: "Jim", rightTitle: "Halpert", bgColor: "#f7f38f" },
+];
+
+let items = [
+  // {
+  //   bgColor: "#f17373",
+  //   end: 1595948760000,
+  //   group: 1,
+  //   id: "140",
+  //   start: 1595941440000,
+  //   title: "jhkj",
+  // },
+];
+let csvData = [];
 
 function App() {
+  let [item, setItem] = useState(items);
+  let [group, setGroup] = useState(groups);
+  const setItemsWithCSV = (data) => {
+    csvData = [];
+    csvData.push(["Driver", "Type", "Location", "Start Time", "End Time"]);
+    data.map((item) => {
+      let driverName = `${groups[parseInt(item.group) - 1].title} ${
+        groups[parseInt(item.group) - 1].rightTitle
+      }`;
+      let type =
+        item.bgColor === "#f17373"
+          ? "Pickup"
+          : item.bgColor === "#72ff72"
+          ? "Dropoff"
+          : "Other";
+      let starttime = moment(item.start, "x").format("DD MMM YYYY hh:mm a");
+      let endtime = moment(item.end, "x").format("DD MMM YYYY hh:mm a");
+
+      csvData.push([driverName, type, item.title, starttime, endtime]);
+    });
+    setItem(data);
+  };
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <NavDropdown title="Choose a Driver" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#calendar/1">
-                Michael Scott
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#calendar/2">
-                Dwight Schrute
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#calendar/3">
-                Jim Halpert
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Nav setItem={setItemsWithCSV} items={item} />
+      <Calendar setItems={setItemsWithCSV} items={item} groups={group} />
+      <div className="row m-5 d-block">
+        <b>
+          <CSVLink data={csvData}>Download Schedule as CSV</CSVLink>
+        </b>
+      </div>
     </div>
   );
 }
