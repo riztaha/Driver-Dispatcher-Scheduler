2121cf303342c6c87e8ff24502466f96ca609887
Taha Rizvi <riztaha@gmail.com>
2020-07-29 19:43:43 -0400

    Example from library for resizing

--- src/CustomTimeline.jsx
+++ src/CustomTimeline.jsx
@@ -1,3 +1,5 @@
+// This code is from the React Calendar Timeline's example: https://codesandbox.io/s/q3rkx1478q?file=/CustomTimeline.jsx
+
 // import React, { Component } from "react";
 // import moment from "moment";
 
