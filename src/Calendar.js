946335fda994d1e110cd23cf0390c2540d30c3cf
Taha Rizvi <riztaha@gmail.com>
2020-07-29 19:43:31 -0400

    Time slots are now being set in calendar, and can be moved/resized.

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,99 +1,129 @@
-import React from "react";
-import AvailableTimes from "react-available-times";
+import React, { useState } from "react";
+import moment from "moment";
 
 import Timeline from "react-calendar-timeline";
-// make sure you include the timeline stylesheet or the timeline will not be styled
-import "react-calendar-timeline/lib/Timeline.css";
-import moment from "moment";
-import CustomTimeline from "./CustomTimeline";
 
-function Calendar() {
-  //   const groups = [
-  //     { id: 1, title: "Michael Scott" },
-  //     { id: 2, title: "Jim Halpert" },
-  //     { id: 3, title: "Dwight Schrute" },
-  //   ];
+const keys = {
+  groupIdKey: "id",
+  groupTitleKey: "title",
+  groupRightTitleKey: "rightTitle",
+  itemIdKey: "id",
+  itemTitleKey: "title",
+  itemDivTitleKey: "title",
+  itemGroupKey: "group",
+  itemTimeStartKey: "start",
+  itemTimeEndKey: "end",
+  groupLabelKey: "title",
+};
 
-  //   const items = [
-  //     {
-  //       id: 1,
-  //       group: 1,
-  //       title: "Pickup",
-  //       start_time: moment(),
-  //       end_time: moment().add(1, "hour"),
-  //     },
-  //     {
-  //       id: 2,
-  //       group: 2,
-  //       title: "Other",
-  //       start_time: moment().add(-1.5, "hour"),
-  //       end_time: moment().add(0.5, "hour"),
-  //     },
-  //     {
-  //       id: 3,
-  //       group: 1,
-  //       title: "Dropoff",
-  //       start_time: moment().add(2, "hour"),
-  //       end_time: moment().add(3, "hour"),
-  //     },
-  //   ];
-  //   return (
-  //     <div>
-  //       {/* <CustomTimeline /> */}
-  //       <Timeline
-  //         groups={groups}
-  //         items={items}
-  //         defaultTimeStart={moment().add(-12, "hour")}
-  //         defaultTimeEnd={moment().add(12, "hour")}
-  //       />
-  //     </div>
+function Calendar(props) {
+  const [defaultTimeStart, setDefaultTimeStart] = useState(
+    moment().startOf("day").toDate()
+  );
+  const [defaultTimeEnd, setDefaultTimeEnd] = useState(
+    moment().startOf("day").add(1, "day").toDate()
+  );
+
+  const handleItemMove = (itemId, dragTime, newGroupOrder) => {
+    const { items, groups } = props;
+
+    const group = groups[newGroupOrder];
+
+    props.setItems(
+      items.map((item) =>
+        item.id === itemId
+          ? Object.assign({}, item, {
+              start: dragTime,
+              end: dragTime + (item.end - item.start),
+              group: group.id,
+            })
+          : item
+      )
+    );
+
+    console.log("Moved", itemId, dragTime, newGroupOrder);
+  };
+
+  const handleItemResize = (itemId, time, edge) => {
+    const { items } = props;
+
+    props.setItems(
+      items.map((item) =>
+        item.id === itemId
+          ? Object.assign({}, item, {
+              start: edge === "left" ? time : item.start,
+              end: edge === "left" ? item.end : time,
+            })
+          : item
+      )
+    );
+
+    console.log("Resized", itemId, time, edge);
+  };
+
+  const itemRenderer = ({
+    item,
+    timelineContext,
+    itemContext,
+    getItemProps,
+    getResizeProps,
+  }) => {
+    const { left: leftResizeProps, right: rightResizeProps } = getResizeProps();
+    const backgroundColor = itemContext.selected
+      ? itemContext.dragging
+        ? "red"
+        : "yellow" //selectedBgColor
+      : item.bgColor;
+    const borderColor = itemContext.resizing ? "red" : item.color;
+    return (
+      <div
+        {...getItemProps({
+          style: {
+            backgroundColor: backgroundColor,
+            color: item.color,
+          },
+          onMouseDown: () => {
+            console.log("on item click", item);
+          },
+        })}
+      >
+        {itemContext.useResizeHandle ? <div {...leftResizeProps} /> : null}
+
+        <div
+          style={{
+            height: itemContext.dimensions.height,
+            overflow: "hidden",
+            paddingLeft: 3,
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+          }}
+        >
+          {itemContext.title}
+        </div>
+
+        {itemContext.useResizeHandle ? <div {...rightResizeProps} /> : null}
+      </div>
+    );
+  };
+
   return (
-    <div>
-      <AvailableTimes
-        weekStartsOn="Sunday"
-        calendars={[
-          {
-            id: "pickup",
-            title: "Pickup",
-            foregroundColor: "#ff00ff",
-            backgroundColor: "#f0f0f0",
-            selected: true,
-          },
-          {
-            id: "dropoff",
-            title: "Dropoff",
-            foregroundColor: "#ff00ff",
-            backgroundColor: "#f3f3f3",
-          },
-          {
-            id: "other",
-            title: "Other",
-            foregroundColor: "#ff00ff",
-            backgroundColor: "#f3f3f3",
-          },
-        ]}
-        onChange={(selections) => {
-          selections.forEach(({ start, end }) => {
-            console.log("Start:", start, "End:", end);
-          });
-        }}
-        // onEventsRequested={({ calendarId, start, end, callback }) => {
-        //   loadMoreEvents(calendarId, start, end).then(callback);
-        // }}
-        height={600}
-        recurring={false}
-        availableDays={[
-          "sunday",
-          "monday",
-          "tuesday",
-          "wednesday",
-          "thursday",
-          "friday",
-          "saturday",
-        ]}
-        availableHourRange={{ start: 0, end: 24 }}
-      />
-    </div>
+    <Timeline
+      groups={props.groups}
+      items={props.items}
+      keys={keys}
+      fullUpdate
+      itemTouchSendsClick={false}
+      sidebarContent={<div>Above The Left</div>}
+      stackItems={true}
+      itemHeightRatio={0.75}
+      canMove={true}
+      canResize={"both"}
+      itemRenderer={itemRenderer}
+      defaultTimeStart={defaultTimeStart}
+      defaultTimeEnd={defaultTimeEnd}
+      onItemMove={handleItemMove}
+      onItemResize={handleItemResize}
+    />
   );
 }
 
