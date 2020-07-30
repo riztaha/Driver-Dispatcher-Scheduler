eb06556f8d248d39897c05f151545e3d352e77bb
Taha Rizvi <riztaha@gmail.com>
2020-07-30 03:24:59 -0400

    Created and then refactored a helperfunction for duplicates

---
+++ src/HelperFunctions.js
@@ -0,0 +1,12 @@
+export default function checkDuplicate(id, requestedTime, data) {
+  for (const previousBooking of data) {
+    console.log(previousBooking);
+    if (
+      parseInt(previousBooking.group) === id &&
+      requestedTime >= previousBooking.start &&
+      requestedTime <= previousBooking.end
+    ) {
+      return parseInt(previousBooking.id);
+    }
+  }
+}
